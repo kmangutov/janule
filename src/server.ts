3e83120da984b8447050cb4dd8bb26a3804f7d04
Kirill Mangutov <kmangutov@gmail.com>
2020-05-31 17:18:15 -0700

    nits

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-
 import { Models } from './types';
 
 // Format from http://bl.ocks.org/jose187/4733747
+// Given Memes, convert to graph format. 
 const memesToDag = async (memes) => {
-
     var nodeUuidToNameMap = {}
     var memeToInt = {}
     var counter = 0
@@ -11,7 +10,7 @@ const memesToDag = async (memes) => {
     // Each node must have an integer index. Make a list of nodes and use counter to create an index.
     // Node name should be the meme text, so we must also keep a Uuid map to build edge graph.
     var nodes = []
-    Array.prototype.forEach.call(memes, (meme) => {
+    memes.forEach((meme) => {
         nodeUuidToNameMap[meme._id] = meme.name
 
         var newMemeIntId = counter++
